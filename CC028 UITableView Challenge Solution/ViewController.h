dab7a1982477578a36f575243a79a60685e3a683
systemtest <systemtest@Profile-Manager.local>
2015-05-27 15:41:40 +0530

    Initial Commit

---
+++ CC028 UITableView Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CC028 UITableView Challenge Solution
+//
+//  Created by systemtest on 27/05/15.
+//  Copyright (c) 2015 PurpleTalk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
