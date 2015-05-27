135067be0cd4250c655608db0fab23a502b8f631
systemtest <systemtest@Profile-Manager.local>
2015-05-27 15:54:08 +0530

    Final Commit

--- CC028 UITableView Challenge Solution/CCTableViewController.h
+++ CC028 UITableView Challenge Solution/CCTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CCTableViewController.h
 //  CC028 UITableView Challenge Solution
 //
 //  Created by systemtest on 27/05/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface CCTableViewController : UITableViewController
 
 @end
-
